ca5da15b7ab379f0b49a8a387e82d3ebe9275ea3
Harrison Jansen van Beek <156035437+harrisonjansenva@users.noreply.github.com>
2025-04-08 16:37:54 -0600

    Update binary.v

--- binary.v
+++ binary.v
@@ -4,9 +4,40 @@ module binary (
     input clk,
     input reset,
     output z,
-    output [3:0] state
+    output[2:0] state
+    
 );
-wire Anext, Bnext, Cnext, Dnext;
+wire [2:0] next;
+
+dff bzero(
+    .Default(1'b1),
+    .D(next[0]),
+    .clk(clk),
+    .Q(state[0])
+    );
+    
+dff bone (
+    .Default(1'b0),
+    .D(next[1]),
+    .clk(clk),
+    .Q(state[1])
+    );
+    
+dff btwo (
+    .Default(1'b0),
+    .D(next[2]),
+    .clk(clk),
+    .Q(state[2])
+    );
+    
+    assign next[0] = (state[0] & ~w) | (state == 3'b011 & ~w) | (state == 3'b100 & ~w);
+    assign next[1] = (state == 3'b001 & ~w) | (state == 3'b010 & ~w);
+    assign next[2] = (state == 3'b011 & w) | (state == 3'b100);
+    
+    assign z = (state == 3'b010) | (state == 3'b100); 
+    
+    
+
 
 
 endmodule
