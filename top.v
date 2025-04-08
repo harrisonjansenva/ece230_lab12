f0e9370761ff88f93ab8c340ac238172e58d50e1
Harrison Jansen van Beek <156035437+harrisonjansenva@users.noreply.github.com>
2025-04-08 16:38:19 -0600

    Update top.v

--- top.v
+++ top.v
@@ -5,6 +5,26 @@ module top(
     input btnU // reset
 );
 
+
+onehot onehotmod (
+    .w(sw),
+    .clk(btnC),
+    .reset(btnU),
+    .z(led[0]),
+    .state(led[6:2])
+    );
+binary binarymod(
+    .w(sw),
+    .clk(btnC),
+    .z(led[1]),
+    .reset(btnU),
+    .state(led[9:7])
+    );
+
+
+
+    
+
     // Hook up binary and one-hot state machines
 
 endmodule
