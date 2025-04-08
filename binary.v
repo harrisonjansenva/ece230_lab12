85ec9a05c58c3275175e083c70c7cc0c40279b0a
Harrison Jansen van Beek <harrisonjansenva@u.boisestate.edu>
2025-04-08 14:50:35 -0600

    implemented onehot.v

--- binary.v
+++ binary.v
@@ -1,1 +1,13 @@
 // Implement binary state machine
+module binary (
+    input w,
+    input clk,
+    input reset,
+    output z,
+    output [3:0] state
+);
+wire Anext, Bnext, Cnext, Dnext;
+
+
+endmodule
+
--- onehot.v
+++ onehot.v
@@ -1,1 +1,53 @@
-// Implement one-hot state machine
+module onehot (
+    input w,
+    input clk,
+    input reset,
+    output z,
+    output [4:0] state
+);
+wire Anext, Bnext, Cnext, Dnext, Enext;
+
+dff A(
+    .Default(1'b1),
+    .D(Anext),
+    .clk(clk),
+    .reset(reset),
+    .Q(state[0])
+);
+dff B(
+    .Default(1'b0),
+    .D(Bnext),
+    .clk(clk),
+    .reset(reset),
+    .Q(state[1])
+);
+dff C(
+    .Default(1'b0),
+    .D(Cnext),
+    .clk(clk),
+    .reset(reset),
+    .Q(state[2])
+);
+dff D(
+    .Default(1'b0),
+    .D(Dnext),
+    .clk(clk),
+    .reset(reset),
+    .Q(state[3])
+);
+dff E(
+    .Default(1'b0),
+    .D(Enext),
+    .clk(clk),
+    .reset(reset),
+    .Q(state[4])
+);
+assign z = state[4] | state[2];
+assign Anext = 1'b0;
+assign Bnext = (state[0] & ~w) | (state[3] & ~w) | (state[4] & ~w);
+assign Cnext = (state[1] & ~w) | (state[2] & ~w);
+assign Dnext = (state[0] & w) | (state[1] & w) | (state[2] & w);
+assign Enext = (state[3] & w) | (state[4] & w);
+
+endmodule
+
